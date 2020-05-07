0e7d2e878fc4523484b1f113cdba1be0abbfe8e5
sajanav <vsajana@gmail.com>
2020-05-07 12:10:15 +0400

    test
    tes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,19 +2,37 @@
 
 console.log('app.js is running');
 //jsx javascript XML 
+var user = {
+    name1: 'Sajana',
+    name2: 'vijayan'
+};
 var template = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
+    'div',
     null,
-    'My First App'
-  ),
-  React.createElement(
-    'p',
-    null,
-    'This is some info'
-  )
+    React.createElement(
+        'h1',
+        null,
+        'My First App'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This is some info'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            user.name1
+        ),
+        React.createElement(
+            'li',
+            null,
+            user.name2
+        )
+    )
 );
 //var template = /*#__PURE__*/React.createElement("p", null, "this is JSX from app.js");
 var appRoot = document.getElementById('app');
--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
 console.log('app.js is running');
 //jsx javascript XML 
-var template=<div><h1>My First App</h1><p>This is some info</p></div>;
+var user={
+    name1:'Sajana',
+    name2:'vijayan'
+};
+var template=(<div>
+    <h1>My First App</h1>
+    <p>This is some info</p>
+    <ol>
+        <li>
+        {user.name1}
+        </li>
+        <li>
+        {user.name2}
+        </li>
+    </ol>
+    </div>
+    );
 //var template = /*#__PURE__*/React.createElement("p", null, "this is JSX from app.js");
 var appRoot=document.getElementById('app');
 ReactDOM.render(template,appRoot);
